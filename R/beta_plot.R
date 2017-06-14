eea324eccd57a995e41fe3ca178b30a45e251276
nmbrodnax <nmbrodnax@gmail.com>
2017-06-14 11:47:24 -0400

    initial commit

---
+++ R/beta_plot.R
@@ -0,0 +1,16 @@
+#' @import ggplot2
+#' @export
+
+beta_plot <- function(n = 10000, a = 1, b = 3) {
+  # draw distributions
+  sims <- rbeta(n = n, shape1 = a, shape2 = b)
+
+  # convert to data frame for ggplot2 compatability
+  sims <- data.frame(x = sims)
+
+  # plot probability density function
+  ggplot(sims, aes(x)) +
+    geom_density() +
+    xlab("") + ylab("Probability Density Function") +
+    theme_bw()
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+RCTR
+====
+
+NaLette Brodnax
+
+Motivation
+----------
+
+This is a test.
+
+Examples
+--------
+
+The `beta_plot` function allows you to simulate data from a beta
+distribution and plot the results.
+
+    library(rctr)
+    beta_plot(a = 1, b = 3)
+
+![](man/figures/unnamed-chunk-1-1.png)
